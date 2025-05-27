f87241b28edc74cbbdd88c03cbddaca8649f7b63
Deadmockey <deadmockey@protonmail.com>
2025-05-26 20:55:28 -0500

    Fully random spinning + coins

--- script.js
+++ script.js
@@ -1,25 +1,31 @@
 const delay = ms => new Promise(res => setTimeout(res, ms));
 
 let spin_btn = document.getElementsByClassName("spin-btn")
-let slot1 = document.getElementById("slot1")
-let slot2 = document.getElementById("slot2")
-let slot3 = document.getElementById("slot3")
+let slot_1 = document.getElementById("slot-1")
+let slot_2 = document.getElementById("slot-2")
+let slot_3 = document.getElementById("slot-3")
 
+// The weighted chance of a symbol appearing
+// 0 means it will randomize as I know no
+// better way of implementing this lmao
 const symbols = [{
+    symbol: 0,
+    chance: 0.30
+}, {
     symbol: 1,
-    chance: 0.45
+    chance: 0.20
 },
 {
     symbol: 2,
-    chance: 0.2
+    chance: 0.17
 },
 {
     symbol: 3,
-    chance: 0.15
+    chance: 0.13
 },
 {
     symbol: 4,
-    chance: 0.1
+    chance: 0.10
 },
 {
     symbol: 5,
@@ -30,9 +36,9 @@ const symbols = [{
     chance: 0.03
 }]
 
-spin_btn[0].addEventListener("click", spin_slots)
+spin_btn[0].addEventListener("click", play_game)
 
-
+// Returns the symbol chosen from random
 function weighted_spin() {
     let rand = Math.random()
     for (let i = 0; i < symbols.length; i++) {
@@ -44,29 +50,51 @@ function weighted_spin() {
     }
 }
 
-async function spin_slots() {
-    let time = 500
+// Does the actual animation of the spin
+async function spin_slots(symbol_1, symbol_2, symbol_3) {
+    const time = 500
+
+    // Randomizes the numbers to make it look like its spinning
     for (let i = 0; i < 30; i++) {
-        slot1.textContent = Math.ceil(Math.random() * 7)
-        slot2.textContent = Math.ceil(Math.random() * 7)
-        slot3.textContent = Math.ceil(Math.random() * 7)
+        slot_1.textContent = Math.ceil(Math.random() * 6)
+        slot_2.textContent = Math.ceil(Math.random() * 6)
+        slot_3.textContent = Math.ceil(Math.random() * 6)
         await delay(Math.sqrt(time))
         time += 150
     }
-    slot1.textContent = weighted_spin()
+    slot_1.textContent = symbol_1
 
     for (let i = 0; i < 25; i++) {
-        slot2.textContent = Math.ceil(Math.random() * 7)
-        slot3.textContent = Math.ceil(Math.random() * 7)
+        slot_2.textContent = Math.ceil(Math.random() * 6)
+        slot_3.textContent = Math.ceil(Math.random() * 6)
         await delay(Math.sqrt(time))
-        time += 250
+        time += 200
     }
-    slot2.textContent = weighted_spin()
+    slot_2.textContent = symbol_2
 
     for (let i = 0; i < 15; i++) {
-        slot3.textContent = Math.ceil(Math.random() * 7)
+        slot_3.textContent = Math.ceil(Math.random() * 6)
         await delay(Math.sqrt(time))
         time += 600
     }
-    slot3.textContent = weighted_spin()
+    slot_3.textContent = symbol_3
+}
+
+async function play_game() {
+    let symbol_1 = weighted_spin()
+    let symbol_2 = symbol_1
+    let symbol_3
+
+    // This Spaghetti code makes sure, when the user loses 
+    // the output is randomized
+    if (symbol_1 == 0) {
+        symbol_1 = Math.ceil(Math.random() * 6)
+        symbol_2 = Math.ceil(Math.random() * 6)
+        symbol_3 = Math.ceil(Math.random() * 6)
+        while (symbol_1 == symbol_3) {
+            symbol_3 = Math.ceil(Math.random() * 6)
+        }
+    }
+
+    spin_slots(symbol_1, symbol_2, symbol_3)
 }
